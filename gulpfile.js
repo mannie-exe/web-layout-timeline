ce8fe607102556d87278b809e718093ae36198a5
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-04 12:03:14 -0400

    Setup basic gulp file watch for Sass compilation

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', () => {
+  return gulp.src([
+    './scss/lib/*.scss',
+    './scss/app.scss'
+  ])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    }
+  });
+});
+
+gulp.task('watch', [ 'browserSync', 'sass' ], () => {
+  gulp.watch([
+    './scss/lib/*.scss',
+    './scss/app.scss',
+    'index.html'
+  ], [ 'sass' ]);
+});
